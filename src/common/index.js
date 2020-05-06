7ffcd8b6fcef18ad938cf9038bf7cacee0dfe0e2
Ahmed Sabry <abou7mied.soft@gmail.com>
2020-05-06 17:17:20 +0200

    Add missing file

---
+++ src/common/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TYPES: {
+    SubscriptionController: 'SubscriptionController',
+    SubscriptionDAL: 'SubscriptionDAL',
+    Validation: 'Validation',
+    Mailer: 'Mailer',
+    Database: 'Database',
+  },
+};
